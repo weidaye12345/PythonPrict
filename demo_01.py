60cbb8de1b5ccd365e3dca62a7abe67414fb34a1
weidaye12345 <15082835841@163.com>
2020-04-11 20:16:12 +0800

    第一次使用github托管代码

---
+++ demo_01.py
@@ -0,0 +1,28 @@
+"""print("hello,word")"""
+"""
+print(233)
+print(2.33)
+print(True)
+print(())
+print([])
+print({})
+
+"""
+"""
+喜喜
+a
+"""
+"""print(1+2>4)"""
+# python默认为字符串,当出现连接符+号时:为拼接状态 
+# 长的像的数据才能转换
+# a= float(input("请输入:"))
+# b= float(input("请输入:"))
+# print("input获取的内容: ",a+b)
+
+
+"""a="asasasadsakhbdahvcdukvckdsjvhcjsdhv"
+print(len(a))"""
+
+a=input("请输入: ")
+b=input("请输入: ")
+print(len(a)+len(b))
